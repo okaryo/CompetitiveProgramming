8fa5e27d91552cb32da14d8cf933bebcb131cf16
okaryo <okaryotr@gmail.com>
2019-06-05 01:18:45 +0900

    add new problem

--- AtCoder/BeginnerContest/021~030/021/C.cpp
+++ AtCoder/BeginnerContest/021~030/021/C.cpp
@@ -2,10 +2,39 @@
 typedef long long LL;
 using namespace std;
 
-int N, M, a, b;
-int mod = 1e9 + 7;
+const int mod = 1e9 + 7;
 
 int main() {
-  cin >> N >> a >> b >> M;
+  int n, a, b;
+  cin >> n >> a >> b;
+  a --, b --;
 
+  int m;
+  cin >> m;
+  vector<vector<int>> g(n, vector<int>(n, 0));
+  for (int i = 0; i < m; i ++) {
+    int x, y;
+    cin >> x >> y;
+    x --, y --;
+    g[x][y] = g[y][x] = 1;
+  }
+
+  vector<vector<int>> dp(n, vector<int>(n));
+  dp[0][a] = 1;
+  for (int cnt = 1; cnt < n; cnt ++) {
+    for (int i = 0; i < n; i ++) {
+      for (int j = 0; j < n; j ++) {
+        if (g[j][i]) {
+          dp[cnt][i] = (dp[cnt - 1][j] + dp[cnt][i]) % mod;
+        }
+      }
+    }
+  }
+
+  for (int i = 0; i < n; i ++) {
+    if (dp[i][b] > 0) {
+      cout << dp[i][b] << endl;
+      break;
+    }
+  }
 }
--- AtCoder/BeginnerContest/121~130/128/D.cpp
+++ AtCoder/BeginnerContest/121~130/128/D.cpp
@@ -3,4 +3,27 @@ typedef long long LL;
 using namespace std;
 
 int main() {
+  int n, k;
+  cin >> n >> k;
+  vector<int> v(n);
+  for (int i = 0; i < n; i ++) cin >> v[i];
+
+  int ans = -5 * 1e8;
+  for (int a = 0; a <= min(n, k); a ++) {
+    for (int b = 0; a + b <= min(n, k); b ++) {
+      vector<int> d;
+      int tmp = 0;
+      for (int c = 0; c < n; c ++) {
+        if (c < a || c > n - b - 1) {
+          tmp += v[c];
+          if (v[c] < 0) d.push_back(v[c]);
+        }
+      }
+      sort(d.begin(), d.end());
+      for (int x = 0; x < min(k - (a + b), (int)d.size()); x ++) tmp -= d[x];
+      ans = max(ans, tmp);
+    }
+  }
+
+  cout << ans << endl;
 }
