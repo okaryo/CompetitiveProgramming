d4ea0aeef43e46eb9b70d93546a1425d9735f181
okaryo <okaryotr@gmail.com>
2019-06-26 00:46:07 +0900

    add new problem

--- AtCoder/RegularContest/001~010/002/B.cpp
+++ AtCoder/RegularContest/001~010/002/B.cpp
@@ -2,27 +2,19 @@
 typedef long long LL;
 using namespace std;
 
-int main() {
-  string s; cin >> s;
-  int y = stoi(s.substr(0, 4));
-  int m = stoi(s.substr(5, 2));
-  int d = stoi(s.substr(8, 2));
+int a[13]={0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-  bool uru = false;
-  if (y % 4 == 0 && !(y % 100 == 0 && y % 400 != 0)) uru = true;
+int main() {
+  int y, m, d;
+  scanf("%d/%d/%d", &y, &m, &d);
+
+  if (y % 400 == 0 || (y % 100 != 0 && y % 4 == 0)) a[2] ++;
 
   int j;
   for (int i = m; i <= 12; i ++) {
-    for (j = d; j <= 31; j ++) {
-      if ((j == 2 || j == 4 || j == 6 || j == 9 || j == 11) && j == 31) continue;
-      if (i == 2 && (j == 30 || (j == 29 && !uru))) continue;
-
-      if (y % (i * j) == 0) {
-        cout << y << "/";
-        if (i < 10) cout << 0;
-        cout << i << "/";
-        if (j < 10) cout << 0;
-        cout << j << endl;
+    for (j = d; j <= a[i]; j ++) {
+      if (y % i == 0 && (y / i) % j == 0) {
+        printf("%04d/%02d/%02d\n", y, i, j);
         return 0;
       }
     }
