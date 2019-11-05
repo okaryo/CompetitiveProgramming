12f3bff9039ba322052816c59952ddd531691146
okaryo <okaryotr@gmail.com>
2019-11-05 23:13:59 +0900

    [add] new problem

--- AtCoder/BeginnerContest/141~150/144/B.cpp
+++ AtCoder/BeginnerContest/141~150/144/B.cpp
@@ -5,4 +5,18 @@ typedef long long LL;
 #define all(x) (x).begin(),(x).end()
 
 int main() {
+  int n; cin >> n;
+  bool check = false;
+  for (int i = 1; i <= 9; i ++) {
+    for (int j = 1; j <= 9; j ++) {
+      if (i * j == n) {
+        check = true;
+        break;
+      }
+    }
+    if (check) break;
+  }
+
+  if (check) cout << "Yes" << endl;
+  else cout << "No" << endl;
 }
