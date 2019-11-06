e3e5e05a1f4c72cbbef5301c75567cdbb9cd69b5
okaryo <okaryotr@gmail.com>
2019-11-06 22:34:36 +0900

    [add] new problem

--- AtCoder/BeginnerContest/141~150/144/C.cpp
+++ AtCoder/BeginnerContest/141~150/144/C.cpp
@@ -5,4 +5,12 @@ typedef long long LL;
 #define all(x) (x).begin(),(x).end()
 
 int main() {
+  LL n; cin >> n;
+  LL ans = n;
+  for (LL i = 1; i * i <= n; i ++) {
+    if (n % i == 0) {
+      ans = min(ans, (i - 1) + (n / i - 1));
+    }
+  }
+  cout << ans << endl;
 }
