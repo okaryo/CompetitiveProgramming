4b08b42dbd17146d166805f2ae2fa2b5b66ddbc6
okaryo <okaryotr@gmail.com>
2019-11-30 10:03:56 +0900

    [add] new problem

--- AtCoder/BeginnerContest/141~150/146/B.cpp
+++ AtCoder/BeginnerContest/141~150/146/B.cpp
@@ -5,4 +5,13 @@ typedef long long LL;
 #define all(x) (x).begin(),(x).end()
 
 int main() {
+  int n; cin >> n;
+  string s; cin >> s;
+  for (int i = 0; i < s.size(); i ++) {
+    char t = s[i] + n;
+    if (t > 'Z') t -= 26;
+    s[i] = t;
+  }
+
+  cout << s << endl;
 }
