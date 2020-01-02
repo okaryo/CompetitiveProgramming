7d312b9af7a6aec8575f5cbd37fcea669a86a3f9
okaryo <okaryotr@gmail.com>
2020-01-03 00:30:34 +0900

    [add] new problem

--- AtCoder/BeginnerContest/141~150/146/C.cpp
+++ AtCoder/BeginnerContest/141~150/146/C.cpp
@@ -4,5 +4,31 @@ typedef long long LL;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define all(x) (x).begin(),(x).end()
 
+LL digit(LL x) {
+  int res = 0;
+  while (x) {
+    res ++;
+    x /= 10;
+  }
+  return res;
+}
+
 int main() {
+  LL a, b, x;
+  cin >> a >> b >> x;
+
+  LL ans = 0;
+  LL left = 1, right = 1e9, middle;
+  while (left < right) {
+    middle = (left + right) / 2;
+    LL t = a * middle + b * digit(middle);
+    if (x >= t) {
+      ans = max(ans, middle);
+      left = middle + 1;
+    } else {
+      right = middle;
+    }
+  }
+
+  cout << ans << endl;
 }
