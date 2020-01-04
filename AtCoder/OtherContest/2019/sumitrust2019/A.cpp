c6ec3bc665f88d7029d0b971f2b06f6c26b7ca87
okaryo <okaryotr@gmail.com>
2020-01-04 15:16:09 +0900

    [add] new problem

---
+++ AtCoder/OtherContest/2019/sumitrust2019/A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+
+int main() {
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+
+  if (a == c) cout << "0" << endl;
+  else cout << "1" << endl;
+}
