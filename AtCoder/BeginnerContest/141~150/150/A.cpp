519c4ad225f3800d81e84c70637a66da704a6b40
okaryo <okaryotr@gmail.com>
2020-01-11 18:38:20 +0900

    [add] new problem

---
+++ AtCoder/BeginnerContest/141~150/150/A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+
+int main() {
+  int k, x;
+  cin >> k >> x;
+
+  if (k * 500 >= x) cout << "Yes" << endl;
+  else cout << "No" << endl;
+}
