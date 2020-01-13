b8cabb80ec7c99318397670d9080843bdb400b86
okaryo <okaryotr@gmail.com>
2020-01-13 12:19:42 +0900

    [add] new problem

---
+++ AtCoder/BeginnerContest/151~160/151/A.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+
+int main() {
+  char z; cin >> z;
+  cout << char(z + 1) << endl;
+}
