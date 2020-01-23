05d4487bec85e6c334a798fa7ab01b22c15c170e
okaryo <okaryotr@gmail.com>
2020-01-23 19:51:51 +0900

    [add] new problem

---
+++ AtCoder/BeginnerContest/151~160/152/B.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+  for (int i = 0; i < max(a, b); i ++) cout << min(a, b);
+  cout << endl;
+}
