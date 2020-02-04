d5e6c7b25badb9d354d62949f0c4ec2d27dc5ec2
okaryo <okaryotr@gmail.com>
2020-02-04 12:00:00 +0900

    [add] new problem

---
+++ AtCoder/OtherContest/2019/pakencamp_2019_day3/A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+  cout << b - a + 1 << endl;
+}
