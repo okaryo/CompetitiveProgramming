0d1fc9755c6d3ad8ff8c8be132672f1f7e2134a0
okaryo <okaryotr@gmail.com>
2020-02-09 12:00:00 +0900

    [add] new problem

--- AtCoder/BeginnerContest/141~150/150/C.cpp
+++ AtCoder/BeginnerContest/141~150/150/C.cpp
@@ -5,4 +5,22 @@ typedef long long LL;
 #define all(x) (x).begin(),(x).end()
 
 int main() {
+  int n; cin >> n;
+  vector<int> p(n), q(n);
+  rep(i, n) cin >> p[i];
+  rep(i, n) cin >> q[i];
+
+  vector<int> a(n);
+  for (int i = 0; i < n; i ++) {
+    a[i] = i + 1;
+  }
+
+  int pc, qc, count = 0;
+  do {
+    if (p == a) pc = count;
+    if (q == a) qc = count;
+    count ++;
+  } while (next_permutation(all(a)));
+
+  cout << abs(pc - qc) << endl;
 }
