8f3dbf2e66bf5c201139eb6d309d55d7ed391566
okaryo <okaryotr@gmail.com>
2020-02-15 23:04:52 +0900

    [add] new problem

---
+++ AtCoder/BeginnerContest/151~160/154/B.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+
+int main() {
+  string s; cin >> s;
+  for (int i =0; i < s.size(); i ++) cout << 'x';
+  cout << endl;
+}
