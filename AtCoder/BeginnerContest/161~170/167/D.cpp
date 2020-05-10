546b592f774d79ad9b3c5e00c4234ce6e8f816ef
okaryo <okaryotr@gmail.com>
2020-05-10 22:48:21 +0900

    [add] new problem

---
+++ AtCoder/BeginnerContest/161~170/167/D.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+
+int main() {
+  LL n, k;
+  cin >> n >> k;
+  vector<LL> a(n);
+  rep(i, n) {
+    cin >> a[i];
+    a[i] --;
+  }
+}
