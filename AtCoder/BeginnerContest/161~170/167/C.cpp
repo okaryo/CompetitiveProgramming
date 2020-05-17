505100d6f618781f0f3383814e0eaefc1eae18b1
okaryo <okaryotr@gmail.com>
2020-05-17 18:40:38 +0900

    [add] new problem

--- AtCoder/BeginnerContest/161~170/167/C.cpp
+++ AtCoder/BeginnerContest/161~170/167/C.cpp
@@ -5,4 +5,40 @@ typedef long long LL;
 #define all(x) (x).begin(),(x).end()
 
 int main() {
+  int n, m, x;
+  cin >> n >> m >> x;
+  vector<int> c(n);
+  vector<vector<int>> a(n, vector<int>(m));
+  rep(i, n) {
+    cin >> c[i];
+    rep(j, m) {
+      cin >> a[i][j];
+    }
+  }
+
+  LL sum = 1e9;
+  for (int bits = 0; bits < (1 << n); bits ++) {
+    LL t = 0;
+    vector<int> check(m);
+    for (int i = 0; i < n; i ++) {
+      if (bits & (1 << i)) {
+        t += c[i];
+        for (int j = 0; j < m; j ++) {
+          check[j] += a[i][j];
+        }
+      }
+    }
+
+    bool ok = true;
+    for (int i = 0; i < m; i ++) {
+      if (check[i] < x) {
+        ok = false;
+        break;
+      }
+    }
+    if (ok) sum = min(sum, t);
+  }
+
+  if (sum == 1e9) sum = -1;
+  cout << sum << endl;
 }
