2910c59b78ad147afcfe4b9d879401ced8314c1c
okaryo <okaryotr@gmail.com>
2020-06-18 09:37:21 +0900

    [add] new problem

---
+++ AtCoder/BeginnerContest/151~160/160/E.rb
@@ -0,0 +1,6 @@
+x, y, a, b, c = gets.split.map(&:to_i)
+p = gets.split.map(&:to_i)
+q = gets.split.map(&:to_i)
+r = gets.split.map(&:to_i)
+
+puts (p.sort[-x, x] + q.sort[-y, y] + r).sort[-(x + y), x + y].reduce(0) {|i, j| i + j}
