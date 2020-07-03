56f5730350946a1c33e4f7e0a56f9dc0fefa4bfb
okaryo <okaryotr@gmail.com>
2020-07-03 10:40:04 +0900

    [add] new problem

--- AtCoder/OtherContest/2019/AISingProgrammingContest2019/C.rb
+++ AtCoder/OtherContest/2019/AISingProgrammingContest2019/C.rb
@@ -1,2 +1,40 @@
-h,w = gets.split.map(&:to_i)
-s = h.times.map{gets.chomp}
+H,W = gets.split.map(&:to_i)
+S = H.times.map{gets.chomp}
+
+$black = 0
+$white = 0
+$check = Array.new(H){Array.new(W, false)}
+DX = [1, 0, -1, 0]
+DY = [0, 1, 0, -1]
+
+def dfs(x, y)
+  $check[x][y] = true
+  if S[x][y] == '#'
+    $black += 1
+  else
+    $white += 1
+  end
+
+  4.times do |dir|
+    nx = x + DX[dir]
+    ny = y + DY[dir]
+    next if nx < 0 || nx >= H || ny < 0 || ny >= W
+    next if S[x][y] == S[nx][ny]
+    next if $check[nx][ny]
+    dfs(nx, ny)
+  end
+end
+
+ans = 0
+H.times do |i|
+  W.times do |j|
+    next if S[i][j] == '.'
+    next if $check[i][j]
+    $black = 0
+    $white = 0
+    dfs(i, j)
+    ans += $black * $white
+  end
+end
+
+puts ans
