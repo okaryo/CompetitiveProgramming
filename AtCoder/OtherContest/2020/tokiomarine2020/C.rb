d069432554136976cca896c31d675b6b352dedb4
okaryo <okaryotr@gmail.com>
2020-07-04 11:33:58 +0900

    [add] new problem

--- AtCoder/OtherContest/2020/tokiomarine2020/C.rb
+++ AtCoder/OtherContest/2020/tokiomarine2020/C.rb
@@ -1,4 +1,21 @@
 n, k = gets.split.map(&:to_i)
 a = gets.split.map(&:to_i)
 
+con = a.dup
+[k, 41].min.times do |i|
+  imos = Array.new(n, 0)
+  n.times do |j|
+    imos[[0, j - con[j]].max] += 1
+    if j + con[j] + 1 < n
+      imos[j + con[j] + 1] -= 1
+    end
+  end
 
+  1.upto(n-1) do |j|
+    imos[j] += imos[j - 1]
+  end
+
+  con = imos
+end
+
+puts con.join(' ')
