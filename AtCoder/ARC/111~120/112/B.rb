e5b975be3dbd174383aed3327ebc75af224a961d
okaryo <okaryotr@gmail.com>
2021-09-04 17:38:59 +0900

    [add] new problem

--- AtCoder/ARC/111~120/112/B.rb
+++ AtCoder/ARC/111~120/112/B.rb
@@ -1,2 +1,17 @@
-b,c = gets.split.map(&:to_i)
-
+B,C = gets.split.map(&:to_i)
+l = B - C/2
+r = B
+if C >= 2
+  r = B + (C-2)/2
+end
+b = -B
+c = C - 1
+p = b - c/2
+q = b + c/2
+ans = r-l+1+q-p+1
+u = [l,p].max
+v = [r,q].min
+if u <= v
+  ans -= v-u+1
+end
+puts(ans)
