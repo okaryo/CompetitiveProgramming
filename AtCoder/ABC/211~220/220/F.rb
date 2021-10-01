52e776d67bd55ba73cad92c208881a3eab42e522
okaryo <okaryotr@gmail.com>
2021-10-01 23:53:26 +0900

    [add] new problem

--- AtCoder/ABC/211~220/220/F.rb
+++ AtCoder/ABC/211~220/220/F.rb
@@ -5,4 +5,36 @@ graph = Array.new(N){[]}
   graph[u-1] << v-1
   graph[v-1] << u-1
 end
-
+sub = Array.new(N, 1)
+ans = Array.new(N, 0)
+q = [[0, -1, 0]]
+check = Array.new(N, false)
+while !q.empty? do
+  now,p,d = q.shift
+  check[now] = true
+  ans[0] += d
+  graph[now].each do |x|
+    next if x == p || check[x]
+    q.push([x, now, d+1])
+  end
+end
+dfs = lambda do |i, p|
+  graph[i].each do |x|
+    next if x == p
+    dfs.call(x, i)
+    sub[i] += sub[x]
+  end
+end
+dfs.call(0, -1)
+q = [[0, -1]]
+check = Array.new(N, false)
+while !q.empty? do
+  now,p = q.shift
+  check[now] = true
+  graph[now].each do |x|
+    next if x == p || check[x]
+    ans[x] = ans[now] - 2*sub[x] + N
+    q.push([x, now])
+  end
+end
+puts(ans)
